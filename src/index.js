46cc791fce08e70a72bb5a5f8c16e7756fa06bb4
Daniil <negativdaniil4@gmail.com>
2023-07-10 10:30:22 +0300

    templates

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
+import galleryItemTpl from '../templates/gallary-item.handlebars'
+import '../src/css/main.css'
+
 const gallery = document.querySelector('js-gallery');
-console.log(countries)
-function makeGalleryItems(countries){
-    return countries.map((country)=>{
-    return `<li class="gallery__item">
-            <div class="gallery__thumb">
-            <img src="" alt="Прапор " width="320">
-            </div>
-            <h2></h2>
-            <p><b>Столиця: </b></p>
-            <p><b>Валюта: </b></p>
-            <p><b>Офиційна мова: </b> </p>
-            <p><b>Населення:</b> людей</p> 
-        </li>`
-    })
-}
+
+// function makeGalleryItems(countries){
+//     return countries.map((country)=>{
+//     return `<li class="gallery__item">
+//             <div class="gallery__thumb">
+//             <img src="" alt="Прапор " width="320">
+//             </div>
+//             <h2></h2>
+//             <p><b>Столиця: </b></p>
+//             <p><b>Валюта: </b></p>
+//             <p><b>Офиційна мова: </b> </p>
+//             <p><b>Населення:</b> людей</p> 
+//         </li>`
+//     })
+// }
